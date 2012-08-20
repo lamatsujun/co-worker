4c6c854cb8f87bbde1695567f1e19b60f3d6f477
lamatsujun <lassicmatsujun@gmail.com>
2012-08-20 17:39:03 +0900

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoWorker::Application.config.secret_token = '023e93e53234e4c561372ac194ee460c6cb650fe316918fd9e207cb9b2d54b5171ed6b994d98d198ecbab5a908bdf50354d55175e96bc158b237afb8256f3ed1'
